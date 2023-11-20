8e274f689bb6a5165a014dcb4de351f38e47f2db
Asifa-khatoon <asifa441.xwave@gmail.com>
2023-11-20 04:43:49 -0800

    assignment quiz

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'font-awesome/css/font-awesome.min.css';
+
+
+
+import HeroSection from './pages/HomePage/components/heroSection/heroSection';
+import Navbar from './pages/HomePage/components/navbar/navbar';
+
+
+
+
+function App() {
+  return (
+    <div >
+     
+      <Navbar/>
+    <HeroSection/>
+   
+   
+   
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/components/heroSection/heroSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import image1 from "../../../../assets/images/div (1).svg"
+import image2 from "../../../../assets/images/div (2).svg"
+
+const HeroSection =()=>{
+  
+    return(
+        <>
+
+        <div className=""> 
+
+        <img className="w-100" src={image1} alt="" />
+        </div>
+        <div className="bg-color">
+            <div className="container py-2">
+
+                <img src={image2} alt="" />
+            </div>
+
+
+
+
+
+
+
+        </div>
+     
+
+   
+
+        </>
+    )
+};
+export default HeroSection;
